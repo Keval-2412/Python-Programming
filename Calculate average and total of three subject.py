25ec9b0ee0070aec08436daa805271a3968f9feb
Keval-2412 <kevalrp2007@gmail.com>
2026-01-18 19:01:56 +0530

    Add files via upload

---
+++ Calculate average and total of three subject.py
@@ -0,0 +1,9 @@
+a = float(input("Enter marks of subject 1: "))
+b = float(input("Enter marks of subject 2: "))
+c = float(input("Enter marks of subject 3: "))
+
+total = a + b + c
+average = total / 3
+
+print("Total Marks: ", total)
+print("Average of three subjects: ", average)
